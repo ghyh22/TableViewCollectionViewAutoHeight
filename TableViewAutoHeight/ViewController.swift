13633079f3d0e2aef51e3c7dea9243fd6c16b5ff
gonghao <gonghao@yunke.com>
2016-11-15 15:07:33 +0800

    Initial Commit

---
+++ TableViewAutoHeight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewAutoHeight
+//
+//  Created by 龚浩 on 16/11/15.
+//  Copyright © 2016年 龚浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
